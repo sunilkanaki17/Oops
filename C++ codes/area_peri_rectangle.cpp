d8a3fbe75f5b1d922e92e2398644a16696c89957
sunilkanaki17 <sunilkanaki17@gmail.com>
2024-03-11 11:45:42 +0530

    added

---
+++ C++ codes/area_peri_rectangle.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int length=4;
+    int breadth=5;
+
+    int area = length * breadth;
+    int perimeter = 2 * (length + breadth);
+
+    cout<<"Area of Rectangle = "<<area<<endl;
+    cout<<"Perimeter of Rectangle = "<<perimeter<<endl;
+}
---
+++ C++ codes/size_of_data_types.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a=4;
+    float b=2.5;
+    char ch='c';
+    bool ischeck;
+
+    cout<<"Size of"<<endl;
+    cout<<"Int = "<<sizeof(a)<<endl;
+    cout<<"Float = "<<sizeof(b)<<endl;
+    cout<<"Char = "<<sizeof(ch)<<endl;
+    cout<<"Bool = "<<sizeof(ischeck);
+}

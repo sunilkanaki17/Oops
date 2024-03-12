df2eb69ea5f8dd0230f2072c150284c76170969d
sunilkanaki17 <sunilkanaki17@gmail.com>
2024-03-12 09:44:46 +0530

    added

---
+++ C++ codes/cels_to_farhenite.cpp
@@ -0,0 +1,12 @@
+#include<bits\stdc++.h>
+using namespace std;
+
+int main()
+{
+    int c;
+    int f;
+    cout<<"Enter temp in celsius : "<<endl;
+    cin>>c;
+    f = ((c*9)/5) + 32;
+    cout<<"Temperature in Farhenite = "<<f<<endl;
+}

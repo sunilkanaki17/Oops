f8b71ee6d28564f64ea413d2300d26aff631c299
sunilkanaki17 <sunilkanaki17@gmail.com>
2024-03-12 09:45:15 +0530

    add

---
+++ C++ codes/prepost.cpp
@@ -0,0 +1,15 @@
+#include<bits\stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a=4;
+    cout<<"Pre increment = "<<++a<<endl;
+    //int b=4;
+    cout<<"Post increment = "<<a++<<endl;
+    //int c=4;
+    cout<<"Pre decrement = "<<--a<<endl;
+    //int d=4;
+    cout<<"Post decrement = "<<a--<<endl;
+
+}

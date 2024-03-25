4702d0f8e2fd91d14fcb449cd8280e19d1e12acf
sunilkanaki17 <sunilkanaki17@gmail.com>
2024-03-25 08:14:20 +0530

    add

--- C++ codes/transpose_matrix.cpp
+++ C++ codes/transpose_matrix.cpp
@@ -4,7 +4,16 @@ using namespace std;
 int main()
 {
     int a[3][3]={1,2,3,4,5,6,7,8,9};
-
+    cout<<"Matrix : "<<endl;
+    for(int i=0;i<3;++i)
+    {
+        for(int j=0;j<3;++j)
+        {
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<"Transpose of matrix : "<<endl;
     for(int i=0;i<3;++i)
     {
         for(int j=i+1;j<3;++j)

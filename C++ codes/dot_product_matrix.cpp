b5acdbdcd58606811f5ba15a35e1446a185ad58c
sunilkanaki17 <sunilkanaki17@gmail.com>
2024-03-31 20:57:47 +0530

    add

--- C++ codes/dot_product_matrix.cpp
+++ C++ codes/dot_product_matrix.cpp
@@ -7,10 +7,9 @@ int main()
     int a[3][3]={2,3,4,3,4,5,4,5,6};  //size 3x3 --> R1 x C1
     int b[3][3]={1,-3,5,0,2,4,3,0,5}; //size 3x3 --> R2 x C2
     int result[3][3];               //result --> p1*s2
-    int sum;
+    
     for(int i=0;i<3;++i)        //R1
     {
-        
         for(int j=0;j<3;++j)        //C2
         {
             result[i][j]=0;
